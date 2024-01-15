8d8b62d1822f363992ede08a97a80e51793a05b2
Ivan Pađen <49401914+ipadjen@users.noreply.github.com>
2024-01-15 21:38:41 +0100

    Update deprecated triangulate_hole()

--- wrapwrap.cpp
+++ wrapwrap.cpp
@@ -89,7 +89,7 @@ void fillHoles(Mesh& mesh) {
 
     // fill using boundary halfedges
     for(halfedge_descriptor h : border_cycles)
-      PMP::triangulate_hole(mesh, h, CGAL::Emptyset_iterator());
+      PMP::triangulate_hole(mesh, h);
 
     std::cout << "  done" << std::endl;
   }
